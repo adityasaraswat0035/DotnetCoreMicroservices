4ae651eaeace1dd5cf8753a4b131400aa4aaa301
AS <saraswataditya0035@gmail.com>
2021-06-16 19:28:55 +0530

    Created: Product Repository Contract and Implementation With Standard set of methods

---
+++ MangoRestaurant/mango.product.repository/repositories/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using mango.infrstructure.common.Wrappers;
+using mango.product.repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mango.product.repository.repositories
+{
+    public interface ProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Optional<Product>> GetProduct(int productId);
+
+        Task<Optional<Product>> SaveProduct(Product product);
+
+        Task<Optional<Product>> DeleteProduct(Product product);
+    }
+}
