94d8a2a1ba965eafa794849064cd0833a0c50324
AS <saraswataditya0035@gmail.com>
2021-06-16 16:59:40 +0530

    Added: Product Db Context For ORM Mapping

---
+++ MangoRestaurant/mango.product.repository/DbContexts/ProductDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace mango.product.repository.DbContexts
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> dbContextOptions):base(dbContextOptions)
+        {
+
+        }
+    }
+}
