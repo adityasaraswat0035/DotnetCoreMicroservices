4d67f43dec82dcb8839fabd34378eab1f38d1314
AS <saraswataditya0035@gmail.com>
2021-06-16 19:33:18 +0530

    Added: Generic Response Model for API Response

---
+++ MangoRestaurant/mango.product.api/Models/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mango.product.api.Models
+{
+    public class Response<T>
+    {
+        public IEnumerable<KeyValuePair<String, IEnumerable<String>>> ErrorMessages { get; set; } = Enumerable.Empty<KeyValuePair<String, IEnumerable<String>>>();
+        public T Result { get; set; }
+    }
+}
