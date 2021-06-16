f61d47d8d53d19bc3ceec92c61ef0bdf079ae96e
AS <saraswataditya0035@gmail.com>
2021-06-16 19:29:49 +0530

    Added: Datatransfer object to transfer entiry to api layer for product and category

---
+++ MangoRestaurant/mango.product.contract/dtos/ProductDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mango.product.contract.dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public double Price { get; set; }
+        public String Description { get; set; }
+        public CategoryDto Category { get; set; }
+        public String ImageUrl { get; set; }
+    }
+}
