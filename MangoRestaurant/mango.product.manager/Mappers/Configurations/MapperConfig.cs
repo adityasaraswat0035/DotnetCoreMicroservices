52aa7ae704b365cb4f30cef6284bcb0d84a9d444
AS <saraswataditya0035@gmail.com>
2021-06-16 19:32:02 +0530

    Added: AutoMapper Configuration For DTO and Entity Mapping

---
+++ MangoRestaurant/mango.product.manager/Mappers/Configurations/MapperConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using mango.product.contract.dtos;
+using mango.product.repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mango.product.manager.Mappers.Configurations
+{
+    public class MapperConfig
+    {
+        public static IMapper RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(configure =>
+            {
+                configure.CreateMap<ProductDto, Product>().ReverseMap();
+                configure.CreateMap<CategoryDto, Category>().ReverseMap();
+            });
+            return mappingConfig.CreateMapper(); ;
+        }
+    }
+}
