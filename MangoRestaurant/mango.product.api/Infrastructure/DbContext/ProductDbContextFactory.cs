4b58a7b49909bf76d6183ce0fc130edf06a5a3d7
AS <saraswataditya0035@gmail.com>
2021-06-17 11:25:05 +0530

    Added: DesigntimeDbContextFactory for Migration

---
+++ MangoRestaurant/mango.product.api/Infrastructure/DbContext/ProductDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using mango.product.api.db;
+using mango.product.repository.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mango.product.api.Infrastructure.DbContext
+{
+    public class ProductDbContextFactory : IDesignTimeDbContextFactory<ProductDbContext>
+    {
+        public ProductDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ProductDbContext>();
+             optionsBuilder.UseSqlServer("Server=localhost;Database=MangoProductApi;Trusted_Connection=true;MultipleActiveResultSets=true",
+              sqlOptions =>
+              {
+                  sqlOptions.MigrationsAssembly(typeof(ProductApiDatabaseMigration).Assembly.FullName);
+              });
+
+            return new ProductDbContext(optionsBuilder.Options);
+        }
+    }
+}
