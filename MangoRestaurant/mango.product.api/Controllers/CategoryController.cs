f2af91c0bdcfb8c7fd22eea4d01e507d61399cce
AS <saraswataditya0035@gmail.com>
2021-06-17 10:51:43 +0530

    Created: ApiBaseController for Better Result Method Overloading

---
+++ MangoRestaurant/mango.product.api/Controllers/CategoryController.cs
@@ -0,0 +1,47 @@
+﻿using mango.product.api.Controllers.Base;
+using mango.product.contract.contracts;
+using mango.product.contract.dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mango.product.api.Controllers
+{
+    [Route("api/category")]
+    [ApiController]
+    public class CategoryController : ApiBaseController
+    {
+        private CategoryManager categoryManager;
+        public CategoryController(CategoryManager categoryManager)
+        {
+            this.categoryManager = categoryManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await categoryManager.GetCategoriesAsync().ConfigureAwait(false));
+        }
+        [HttpGet]
+        [Route("{categoryId:int}")]
+        public async Task<IActionResult> Get(int categoryId)
+        {
+            return OptionalOK(await categoryManager.GetCategoryAsync(categoryId).ConfigureAwait(false));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(CategoryDto categoryDto)
+        {
+            return Ok(await categoryManager.SaveCategoryAsync(categoryDto).ConfigureAwait(false));
+        }
+        [HttpDelete]
+        [Route("{categoryId:int}")]
+        public async Task<IActionResult> Delete(int categoryId)
+        {
+            await categoryManager.DeleteCategoryAsync(categoryId).ConfigureAwait(false);
+            return NoContent();
+        }
+    }
+}
---
+++ MangoRestaurant/mango.product.api/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using mango.infrstructure.common.Wrappers;
+using mango.product.api.Controllers.Base;
+using mango.product.api.Models;
+using mango.product.contract.contracts;
+using mango.product.contract.dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mango.product.api.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ApiBaseController
+    {
+        private ProductManager productManager;
+        public ProductController(ProductManager productManager)
+        {
+            this.productManager = productManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await productManager.GetProductsAsync().ConfigureAwait(false));
+        }
+        [HttpGet]
+        [Route("{productId:int}")]
+        public async Task<IActionResult> Get(int productId)
+        {
+            return OptionalOK(await productManager.GetProductAsync(productId).ConfigureAwait(false));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(ProductDto productDto)
+        {
+            return Ok(await productManager.SaveProductAsync(productDto).ConfigureAwait(false));
+        }
+        [HttpPut]
+        public async Task<IActionResult> Put(ProductDto productDto)
+        {
+            return Ok(await productManager.SaveProductAsync(productDto).ConfigureAwait(false));
+        }
+        [HttpDelete]
+        [Route("{productId:int}")]
+        public async Task<IActionResult> Delete(int productId)
+        {
+            await productManager.DeleteProductAsync(productId).ConfigureAwait(false);
+            return NoContent();
+        }
+    }
+}
