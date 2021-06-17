215d50b5bf4b6890c4f016cc6e85df97160a8442
AS <saraswataditya0035@gmail.com>
2021-06-17 10:49:08 +0530

    Created:Generic Repository<T,K> And Base Entity

---
+++ MangoRestaurant/mango.product.repository/repositories/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using mango.product.repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mango.product.repository.repositories
+{
+    interface CategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<Category> GetCategoryAsync(int productId);
+
+        Task<Category> SaveCategoryAsync(Product product);
+
+        Task DeleteCategoryAsync(int productId);
+    }
+}
