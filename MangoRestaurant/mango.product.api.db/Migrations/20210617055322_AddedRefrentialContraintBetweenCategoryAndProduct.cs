8d48e2ab4f9e8476103deaf826e27d4b34a5d489
AS <saraswataditya0035@gmail.com>
2021-06-17 11:29:54 +0530

    Added: Migration for  Refrential integrity b/w category and product

---
+++ MangoRestaurant/mango.product.api.db/Migrations/20210617055322_AddedRefrentialContraintBetweenCategoryAndProduct.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mango.product.api.db.Migrations
+{
+    public partial class AddedRefrentialContraintBetweenCategoryAndProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                schema: "dbo",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_CategoryId",
+                schema: "dbo",
+                table: "Product",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_ProductCategory_CategoryId",
+                schema: "dbo",
+                table: "Product",
+                column: "CategoryId",
+                principalSchema: "dbo",
+                principalTable: "ProductCategory",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_ProductCategory_CategoryId",
+                schema: "dbo",
+                table: "Product");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Product_CategoryId",
+                schema: "dbo",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                schema: "dbo",
+                table: "Product");
+        }
+    }
+}
